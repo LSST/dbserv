ed747b57900c9337693955290ac7dd4c695e92de
Christine Banek <cbanek@lsst.org>
2018-08-27 15:58:40 -0700

    [DM-15396] Commit new packaging process.
    This goes to the python packaging process.  Use requirements.txt
    for requirements, setup.cfg and setup.py support running
    python setup.py test.
    
    Version string is made by the setuptools_scm plugin, and we make
    it available the same way that we had it before in the EUPS way.

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup
 
-requires = [
-    'flask',
-    'mysqlclient'
-]
-
 setup(
     name='dbserv',
-    version='0.1',
     package_dir={'lsst': 'python/lsst'},
     package_data={'lsst': ['dax/dbserv/templates/*', 'dax/dbserv/static/*']},
     packages=['lsst', 'lsst.dax.dbserv', 'lsst.dax.dbserv.compat'],
     zip_safe=False,
-    install_requires=requires
+    use_scm_version=True
 )
